a04387fc518b8cf9a6122d44a437c891848e2921
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-04-27 20:00:43 +0500

    feat: init cui
    *init cui
    *added test .txt files

---
+++ cui/cui.py
@@ -0,0 +1,53 @@
+import os
+import curses
+
+
+directory = './Files/'
+files = os.listdir(directory)
+
+
+def print_help_message(*args):
+    print('Available commands:\n'
+          '/exit - to terminate program\n'
+          '/edit "file.txt" - to edit file.txt\n'
+          '/files - to get file names from folder\n'
+          '/help - get help message\n')
+
+
+def clear_screen(*args):
+    os.system("cls")
+
+
+def open_file(*args):
+    file_name = args[0]
+    try:
+        f = open(directory + file_name, 'r', encoding='utf-8').readlines()
+        text = input("\n".join(f))
+    except:
+        print(f'Unable to read file "{file_name}"')
+
+
+def print_files(*args):
+    for file in files:
+        print('*', file)
+
+
+commands = {
+    "exit": exit,
+    "edit": open_file,
+    "files": print_files,
+    "help": print_help_message
+    }
+
+if __name__ == "__main__":
+    clear_screen()
+    print_help_message()
+    while True:
+        input_lines = input('\n/').split()
+        clear_screen()
+        command = input_lines[0]
+        if command in commands.keys():
+            content = "" if len(input_lines) == 1 else input_lines[1]
+            commands[command](content)
+        else:
+            print_help_message()
