5a81a5ee9d1cc865b7612a5bd75c1cf1f5d02aab
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-05-06 22:09:36 +0500

    feat: implement curses module
    * implement curses module
    * added text editor for single person
    * improved code style
    * improve cui

---
+++ cui/config.py
@@ -0,0 +1,11 @@
+all_commands = 'Welcome to online Text Editor!\n' \
+               'Available commands:\n' \
+			   '/files - get all available file names\n' \
+			   '/edit "file.txt" - to edit file.txt ' \
+               '[If requested file doesn\'t exist, it will be created]\n' \
+			   '/help - get help message\n' \
+               '/exit - to terminate program\n'
+
+
+start_message = 'Are you sure you want to edit the file'
+
