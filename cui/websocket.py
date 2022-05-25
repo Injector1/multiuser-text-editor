a5e2a4cf2ac076ea6778f23f1e35dd86026f5459
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-05-25 22:37:15 +0500

    feature: text editor
    * added alpha text editor

--- cui/websocket.py
+++ cui/websocket.py
@@ -1,6 +1,6 @@
 import json
-
 import websocket
+from config import host
 
 
 class WebSocket:
@@ -8,7 +8,13 @@ class WebSocket:
 		self.id = client_id
 		self.file = file_name
 		self.ws = websocket.WebSocket()
-		self.ws.connect(f"ws://localhost:8000/ws/{self.id}&{self.file}")
+		self.ws.connect(f'{host}{client_id}&{file_name}')
 
 	def send_message(self, message: str):
-		self.ws.send(json.dumps(message))
+		self.ws.send(json.dumps(
+			{'type': 'insert',
+			 'value': message}
+		))
+
+	def close_connection(self):
+		self.ws.close()
