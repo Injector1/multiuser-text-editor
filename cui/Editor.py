9e894d6807ee9355143da21f769ef4abf81dfd10
Tolchenov Andrey <jon.ivan2014@yandex.ru>
2022-06-01 23:43:01 +0500

    feat: first version of cui
    * first version of cui is done

--- cui/Editor.py
+++ cui/Editor.py
@@ -1,54 +1,63 @@
-import curses
-from time import sleep
-import keyboard
-import pyautogui
 from WebSocket import WebSocket
-from uuid import uuid1
 from config import *
-from multiprocessing import Process
+
+import curses
+from uuid import uuid1
+from pynput.keyboard import Key, Listener
 
 
 class Editor:
 	def __init__(self, file_name: str):
 		self.file_name = file_name
 		self.is_end = False
-		self.text = self.get_text_from_server()
+		self.text = "".join(open(directory + self.file_name, 'r', encoding='utf-8'))
 		self.ws = WebSocket(str(uuid1()), self.file_name)
 		self.cui = None
 
 	def run(self) -> None:
-		p1 = Process(target=self.start_session)
-		p2 = Process(target=self.update_local_text)
-
-		self.setup_hotkeys()
-
 		self.cui = curses.initscr()
+		self.start_session()
 
-		p1.start()
-		p2.start()
-
-		p1.join()
-		p2.join()
+	def on_press(self, key: Key):
+		if key == Key.esc:
+			self.end_session()
+			return False
+		if key == Key.backspace:
+			self.delete_left_symbol()
+			self.update_server_text()
+		elif key == Key.delete:
+			self.delete_right_symbol()
+			self.update_server_text()
+		elif key == Key.up:
+			self.move_mouse_up()
+			self.update_server_text()
+		elif key == Key.down:
+			self.move_mouse_down()
+		elif key == Key.left:
+			self.move_mouse_left()
+		elif key == Key.right:
+			self.move_mouse_right()
+		elif key == Key.space:
+			self.add_text(' ')
+			self.update_server_text()
+		elif key == Key.enter:
+			self.add_text('\n')
+			self.update_server_text()
+		elif "'" in str(key):
+			self.add_text(str(key)[1])
+			self.update_server_text()
 
 	def start_session(self) -> None:
 		self.cui.addstr(0, 0, self.text)
-		while not self.is_end:
+		with Listener(on_press=self.on_press) as l:
 			self.cui.refresh()
 			self.cui.clrtobot()
-
-			key = self.cui.getkey()
-			if key in '\b':
-				continue
-			if self.is_end:
-				break
-			self.add_text(key)
-			self.update_server_text()
+			l.join()
 
 	def end_session(self) -> None:
 		self.is_end = True
 		self.ws.close_connection()
 		curses.endwin()
-		self.update()
 
 	def add_text(self, key: str) -> None:
 		y, x = self.cui.getyx()
@@ -56,7 +65,7 @@ class Editor:
 			self.text += '\n'
 
 		changed_line = list(self.text.split('\n')[y])
-		changed_line.insert(x - 1, key)
+		changed_line.insert(x, key)
 		changed_line = ''.join(changed_line)
 
 		if y > 0:
@@ -69,13 +78,11 @@ class Editor:
 
 		self.cui.addstr(0, 0, self.text)
 		self.cui.refresh()
-		self.move_mouse(y, x, 0.1)
 
 	def move_mouse_up(self) -> None:
 		y, x = self.cui.getyx()
 		if y > 0:
-			self.cui.move(y - 1, x)
-		self.cui.refresh()
+			self.move_mouse(y - 1, len(self.text.split('\n')[y - 1]))
 
 	def move_mouse_down(self) -> None:
 		y, x = self.cui.getyx()
@@ -87,16 +94,13 @@ class Editor:
 	def move_mouse_left(self) -> None:
 		y, x = self.cui.getyx()
 		if x > 0:
-			self.cui.move(y, x - 1)
-			self.cui.refresh()
+			self.move_mouse(y, x - 1)
 
 	def move_mouse_right(self) -> None:
 		y, x = self.cui.getyx()
-		self.cui.move(y, x + 1)
-		self.cui.refresh()
+		self.move_mouse(y, x + 1)
 
-	def move_mouse(self, y: int, x: int, sec: float) -> None:
-		sleep(sec)
+	def move_mouse(self, y: int, x: int) -> None:
 		self.cui.move(y, x)
 		self.cui.refresh()
 
@@ -114,9 +118,8 @@ class Editor:
 				self.text = ''.join(changed_text) + '\n' + \
 				            '\n'.join(self.text.split('\n')[y + 1:])
 			self.cui.addstr(0, 0, self.text)
-			sleep(0.1)
 			self.cui.refresh()
-			self.move_mouse(y, x - 1, 0.15)
+			self.move_mouse(y, x - 1)
 
 	def delete_right_symbol(self) -> None:
 		y, x = self.cui.getyx()
@@ -132,41 +135,7 @@ class Editor:
 		self.cui.refresh()
 		self.cui.addstr(0, 0, self.text)
 		self.cui.refresh()
-		self.move_mouse(y, x, 0.15)
-
-	def setup_hotkeys(self) -> None:
-		hotkeys = {
-			'esc': self.end_session,
-			'up': self.move_mouse_up,
-			'down': self.move_mouse_down,
-			'left': self.move_mouse_left,
-			'right': self.move_mouse_right,
-			'backspace': self.delete_left_symbol,
-			'delete': self.delete_right_symbol
-		}
-
-		for key, command in hotkeys.items():
-			keyboard.add_hotkey(key, command)
-
-			# def sync(self) -> None:
-			#   self.update_local_text(self.get_text_from_server())
-			#   self.update_server_text(self.text)
+		self.move_mouse(y, x)
 
 	def update_server_text(self) -> None:
 		self.ws.send(self.text)
-		sleep(1)
-
-	def update_local_text(self, text: str) -> None:
-		while not self.is_end:
-			self.cui.addstr(0, 0, text)
-			sleep(0.1)
-			self.cui.refresh()
-			sleep(1)
-
-	def get_text_from_server(self) -> str:
-		with open(directory + self.file_name, 'r', encoding='utf-8') as f:
-			return "".join(f)
-
-	@staticmethod
-	def update() -> None:
-		pyautogui.hotkey('enter')
